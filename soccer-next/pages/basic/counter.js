b2ee22255702d45b06efbbdde43b7a91e587685e
nengcham <nengcham7@gmail.com>
2022-03-30 18:44:16 +0900

    redux start

--- soccer-next/pages/basic/counter.js
+++ soccer-next/pages/basic/counter.js
@@ -1,13 +1,19 @@
 import React, {useState} from "react";
+import {useSelector, useDispatch} from 'react-redux'
+import styles from 'styles/basic/counter.module.css'
 
 export default function Counter(){
-    const [count, setCount] = useState(0)
+    const count = useSelector(selectCount)
+    const dispacch = useDispatch()
+    count [incrementAmount, setIncrementAmount] = useState('0')
+    const incrementValue = Number(incrementAmount) || 0
+
     return(
-        <>
+        <div className={styles.row}>
         <h1>Counter</h1>
         <button onClick={() => setCount(count+1)}> + </button>
         <button onClick={() => setCount(count-1)}> - </button>
-        <div>{count}</div>
-        </>
+        <div className={styles.value}>{count}</div>
+        </div>
     )
 }
